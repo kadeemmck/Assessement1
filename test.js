b06bc8a430fd417aa5e12b5c71a8f593b85159b7
COR Students <corstudents@Matts-MacBook-Pro-57.local>
2018-10-22 12:19:43 -0400

    Assessement complete

---
+++ test.js
@@ -0,0 +1,59 @@
+// // create variable of each following datat types 
+// // string, int, bool, Float
+
+// #1
+let string = "booo"
+let int =  30
+let boolean = true
+let float = 3.14
+
+
+// // #2
+if(trueOrfalse) {
+    return "true";
+
+
+
+//  #3
+ 
+
+for (i=0; i<= 100; i++)
+   if (i%2 === 0){
+       console.log (i)
+   }
+
+// #4
+
+let i = 1;
+while ( i <= 20) {
+	console.log("Hello");
+	i++;
+}
+
+
+  boolean b = false;
+if(b = true) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+
+
+
+
+
+// // #5
+
+function trueOrfalse()
+ if trueOrfalse 
+ 
+
+
+
+
+
+// //  bonus question
+  Dont repeat yourself
+
+
